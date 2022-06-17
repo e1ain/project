70fa544f7647a1b5578c4021381a7aa5e5c0029b
Emily <u981477729@gmail.com>
2022-06-18 01:35:28 +0800

    item、record

--- assets/JS/number.js
+++ assets/JS/number.js
@@ -8,4 +8,13 @@ function addNUM(){
     let value=Number(document.getElementById('num').value);
     document.getElementById('num').value=value+1;
 }
-
+function MinusNUM2(){
+    let value=Number(document.getElementById('num1').value);
+    if(value !=1){
+        document.getElementById('num1').value=value-1;
+    }
+}
+function addNUM2(){
+    let value=Number(document.getElementById('num1').value);
+    document.getElementById('num1').value=value+1;
+}
